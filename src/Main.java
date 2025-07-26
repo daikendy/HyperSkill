5b8cef223e0986faed68df2eaa2c93061e230b79
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-07-26 12:07:12 +0800

    Main file that executes the program

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Question question = new Question();
+    
+        boolean running = true;
+        
+        while (running) {
+            question.showMenu();
+            System.out.print("Enter your choice: ");
+            String choice = scan.nextLine();
+        // Handling user input; only accept valid input 
+            while (!choice.matches("[1-3]")) {
+                System.out.println("Invalid input. Please enter 1, 2, or 3:");
+                choice = scan.nextLine();
+                }
+                int input = Integer.parseInt(choice);
+
+            switch (input) {
+                case 1:
+                    question.multipleChoiceQuestion();
+                    break;
+                case 2:
+                    question.trueOrFalseQuestion();
+                    break;
+                case 3:
+                    question.quitProgram();
+                    running = false; // Exit the loop
+                    break;
+                default:
+                    System.out.println("Invalid choice, try again.");
+            }
+    
+            System.out.println(); // Just for cleaner spacing
+        }
+    
+        System.out.println("Program ended.");
+    }
+}
+
