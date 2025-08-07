4cb0429423dfc9629969442a4af033e5cd3f710e
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-08-07 13:07:46 +0800

    refactor: streamline user input handling by removing Scanner and utilizing avoidInputChoiceError method

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         LogicFlow question = new LogicFlow();
-
         boolean running = true;
         // checks the condition for menu selection
         while (running) {
             question.showMenu();
             System.out.print("Enter your choice: ");
-            String choice = scan.nextLine();
-        // Handling user input; only accept valid input 
-            while (!choice.matches("[1-4]")) {
-                System.out.println("Invalid input. Please enter 1, 2, 3, or 4:");
-                choice = scan.nextLine();
-                }
-                int input = Integer.parseInt(choice);
-
+        // Handling user input; only accept valid input
+        int input = question.avoidInputChoiceError(1,3);
             switch (input) {
                 case 1:
                     System.out.println("--------Choose the correct answer--------");
@@ -25,26 +15,18 @@ public class Main{
                     question.multipleChoiceQuestion();
                     break;
                 case 2:
-                    System.out.println("--------Choose the correct answer--------");
-                    System.out.println();
-                    question.generalMCQ();
-                    break;
-                case 3:
                     System.out.println("--------Answer with True or False--------");
                     System.out.println();
                     question.trueOrFalseQuestion();
                     break;
-                case 4:
+                case 3:
                     question.quitProgram();
                     running = false; // Exit the loop
                     break;
-                default:
-                    System.out.println("Invalid choice, try again.");
             }
     
             System.out.println(); // Just for cleaner spacing
         }
-        scan.close();
         System.out.println("Program ended.");
     }
 }
