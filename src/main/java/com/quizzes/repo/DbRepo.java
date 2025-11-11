b5c1326f53f0d688a9c4fb103d01bf778b8c55e6
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-11-11 21:28:27 +0800

    Made an interface for crud operation with the database

--- src/main/java/com/quizzes/repo/DbRepo.java
+++ src/main/java/com/quizzes/repo/DbRepo.java
@@ -1,4 +1,8 @@
 package com.quizzes.repo;
 
-public interface DbRepo {
-}
+import com.quizzes.model.QuestionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DbRepo<integer> extends JpaRepository<QuestionType, integer> { }
