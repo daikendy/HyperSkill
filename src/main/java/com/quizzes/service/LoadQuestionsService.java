a673e184ec28a632f70c24db79c7884da52b11f4
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-11-14 18:31:49 +0800

    Add @Autowired to the constructor injection

--- src/main/java/com/quizzes/service/LoadQuestionsService.java
+++ src/main/java/com/quizzes/service/LoadQuestionsService.java
@@ -5,31 +5,38 @@ import com.quizzes.repo.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@NoArgsConstructor
 public  class LoadQuestionsService {
 
+    private GeneralMCQRepository genMcq;
+    private  MCQScienceRepository scienceMcq;
+    private MCQGeographyRepository geographyMcq;
+    private TFQuestionRepository tfQuestion;
+
     @Autowired
-    private static GeneralMCQRepository genMcq;
-    @Autowired
-    private static MCQScienceRepository scienceMcq;
-    @Autowired
-    private static MCQGeographyRepository geographyMcq;
-    @Autowired
-    private static TFQuestionRepository tfQuestion;
+    public LoadQuestionsService(GeneralMCQRepository genMcq, MCQScienceRepository scienceMcq, MCQGeographyRepository geographyMcq, TFQuestionRepository tfQuestion) {
+        this.genMcq = genMcq;
+        this.scienceMcq =  scienceMcq;
+        this.geographyMcq =  geographyMcq;
+        this.tfQuestion =  tfQuestion;
+    }
     // Helper method to load questions
-    public  static List<? extends QuestionType> loadQuestions(Class<? extends QuestionType> questionClass) {
+    public  List<? extends QuestionType> loadQuestions(Class<? extends QuestionType> questionClass) {
         if (questionClass.equals(GeneralMCQ.class)){
             return genMcq.findAllByOrderByIdAsc();
         }
-        if (questionClass.equals(MCQScience.class)) {
+        else if (questionClass.equals(MCQScience.class)) {
             return scienceMcq.findAllByOrderByIdAsc();
         }
-        if (questionClass.equals(MCQGeography.class)) {
+        else if (questionClass.equals(MCQGeography.class)) {
             return geographyMcq.findAllByOrderByIdAsc();
         }
-        if (questionClass.equals(TrueOrFalseQuestion.class)) {
+        else if (questionClass.equals(TrueOrFalseQuestion.class)) {
             return tfQuestion.findAllByOrderByIdAsc();
         }
         return new ArrayList<>();
