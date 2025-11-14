70616fa78dcd28e2122a371c3602b73984e2cfab
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-11-14 20:59:02 +0800

    Preapring for RESTful API integration

--- src/main/java/com/quizzes/controller/QuizController.java
+++ src/main/java/com/quizzes/controller/QuizController.java
@@ -1,14 +1,22 @@
 package com.quizzes.controller;
 
+import com.quizzes.model.MCQScience;
+import com.quizzes.model.Questions;
+import com.quizzes.service.LoadQuestionsService;
+import com.quizzes.service.LogicFlow;
+import com.quizzes.service.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/science")
 public class QuizController {
 
-    @RequestMapping("/")
-    public String getQuizzes() {
-        return "Hello, Quizzes!";
+    @RequestMapping("/questions")
+    public String getScience (){
+        return "Kinda confusing this one with RESTful API...";
     }
-
 }
